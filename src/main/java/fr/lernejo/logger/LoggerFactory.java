71637e3ff4dff1a576bfdeefe7fc8d56e65c2aae
mickael.fernandes.0@gmail.com <mickael.fernandes.0@gmail.com>
2021-12-07 17:20:26 +0100

    LoggerFixBug

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,5 +1,10 @@
 package fr.lernejo.logger;
 
-public interface Logger {
-    void log(String message);
+public class LoggerFactory {
+    public static Logger getLogger( String name ) {
+        FileLogger fileLogger = new FileLogger( "./runLogs.log" );
+        FilteredLogger filteredLogger = new FilteredLogger( fileLogger, message -> message.contains( "simulation" ) );
+        CompositeLogger compositeLogger = new CompositeLogger( new ConsoleLogger(), filteredLogger );
+        return new ContextualLogger( name, compositeLogger );
+    }
 }
