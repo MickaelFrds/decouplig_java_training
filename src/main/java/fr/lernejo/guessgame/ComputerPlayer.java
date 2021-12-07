063adc7dae3e296b6b7573c0870787d646353f9e
mickael.fernandes.0@gmail.com <mickael.fernandes.0@gmail.com>
2021-12-07 17:03:21 +0100

    Création d’un utilisateur robot

---
+++ src/main/java/fr/lernejo/guessgame/ComputerPlayer.java
@@ -0,0 +1,30 @@
+package fr.lernejo.guessgame;
+
+import fr.lernejo.logger.LoggerFactory;
+
+public class ComputerPlayer implements Player {
+
+    private Boolean lastGuessLowerOrGreater = null;
+    private Long lowerBound = Simulation.LOWER_BOUND;
+    private Long upperBound = Simulation.UPPER_BOUND;
+
+    @Override
+    public Long askNextGuess() {
+        long nextGuess;
+        if ( lastGuessLowerOrGreater != null ) {
+            if ( lastGuessLowerOrGreater ) {
+                this.lowerBound = ( this.upperBound + this.lowerBound ) / 2;
+            } else {
+                this.upperBound = ( this.upperBound + this.lowerBound ) / 2;
+            }
+        }
+        nextGuess = ( this.upperBound + this.lowerBound ) / 2;
+        return nextGuess;
+    }
+
+    @Override
+    public void respond( boolean lowerOrGreater ) {
+        LoggerFactory.getLogger( "player" ).log( lowerOrGreater ? "Greater" : "Lower" );
+        lastGuessLowerOrGreater = lowerOrGreater;
+    }
+}
